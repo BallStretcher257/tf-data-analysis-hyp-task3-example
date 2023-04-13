91a753dd5ef3ff41f47809da1cada0236cef8278
Alexander <krasnopeev0407@yandex.ru>
2023-04-14 01:12:20 +0500

    attempt one

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
+from statsmodels.stats.weightstats import ztest
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 625760313 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return ztest(x, value = 300, alternative = 'smaller')[1] > 0.02 # Ваш ответ, True или False
